7b0870d19215def4593ba2d0cb6f78d3d6888ab6
Luka Ivanović <luka@ljudi.org>
2025-04-03 23:24:43 +0200

    feat: Add handler and ui for individual pastes

---
+++ routes/[id].tsx
@@ -0,0 +1,51 @@
+import { Head } from "$fresh/runtime.ts";
+import { Handlers } from "$fresh/server.ts"
+import { getDocumentById, Document } from "../lib/db.ts";
+import {CSS as gfmCSS, render} from "@deno/gfm";
+
+export const handler: Handlers = {
+  async GET(_, ctx){
+    const id = ctx.params.id;
+    const document = await getDocumentById(id);
+    if (document === null) {
+      return ctx.renderNotFound();
+    }
+
+    return ctx.render(document)
+  }
+}
+
+export default function Home(props: PageProps<Document>) {
+  const isMarkdown = props.data.title.endsWith(" (md)")
+  return (
+    <>
+      <Head>
+        <title>Paste: {props.data.title}</title>
+      </Head>
+      <div class="p-8 ml-20">
+        <header class="mb-6">
+          <h1 class="text-3xl font-bold text-gray-900 mb-2">
+            {props.data.title}
+          </h1>
+          <time
+            class="text-sm text-gray-500"
+            datetime={props.data.timestamp}
+          >
+            {new Date(props.data.timestamp).toLocaleString()}
+          </time>
+        </header>
+        {isMarkdown ? (
+          <div class="prose prose-slate max-w-3xl"
+            dangerouslySetInnerHTML={{
+              __html: render(props.data.content)
+            }}>
+          </div>
+        ): (
+          <div>
+            {props.data.content}
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
