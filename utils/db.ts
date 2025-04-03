7dffdfb727f587a59c4cb4fa23a294a526295e78
Luka Ivanović <luka@ljudi.org>
2025-04-03 22:26:46 +0200

    feat: Add db operations

---
+++ utils/db.ts
@@ -0,0 +1,23 @@
+import { nanoid } from "@sitnik/nanoid";
+
+const kv = await Deno.openKv();
+
+export interface Document {
+  title: string;
+  content: string;
+  timestamp: number;
+}
+
+export async function saveDocument(
+  title: string,
+  content: string,
+): Promise<string> {
+  const id = nanoid();
+  await kv.set(["documents", id], { title: title, content: content, timestamp: Date.now() });
+  return id;
+}
+
+export async function getDocumentById(id: string): Promise<Document | null> {
+  const document = await kv.get<Document>(["documents", id]);
+  return document.value;
+}
