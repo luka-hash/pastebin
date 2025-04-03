9be95cddac6c0a4b1ff1e03f5ce665679483b9b0
Luka Ivanović <luka@ljudi.org>
2025-04-03 22:43:14 +0200

    feat: Add UI and basic handlers

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,42 @@
+import { Head } from "$fresh/runtime.ts";
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  async GET(_, ctx) {
+    return ctx.render()
+  },
+  async POST(req) {
+    const form = await req.formData();
+    console.log(form.get("title"), form.get("content"))
+    return new Response("", {
+      status: 302,
+      headers: {
+        Location: "/",
+      }
+    })
+  }
+}
+
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+    <>
+      <Head>
+        <title>Pastebin</title>
+      </Head>
+      <div class="px-4 py-8 mx-auto bg-[#86efac] h-screen">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <h1 class="text-4xl font-bold">Welcome to Pastebin</h1>
+          <form action="/" method="post" class="mt-8 flex flex-col gap-2">
+            <input
+              type="text"
+              name="title"
+              placeholder="Enter paste title..."
+            />
+            <textarea name="content">Enter paste content here</textarea>
+            <button type="submit" class="bg-[#aaaaaa]">Submit paste</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
